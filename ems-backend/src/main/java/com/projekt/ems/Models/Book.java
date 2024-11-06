97b23330e3b6b5b9d1e2f637688041b737f3b5dc
Avikele <konrad.malarczyk1@gmail.com>
2024-11-06 13:08:38 +0100

    /feat added new entities

---
+++ ems-backend/src/main/java/com/projekt/ems/Models/Book.java
@@ -0,0 +1,58 @@
+package com.projekt.ems.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "pages", nullable = false)
+    private Integer pages;
+
+    @Column(name = "cover")
+    private String cover;
+
+    @Column(name = "status")
+    private boolean status;
+
+    @Column(name = "avgRating")
+    private Double AvgRating;
+
+    @ManyToMany
+    @JoinTable(
+            name = "book_has_author",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id")
+    )
+    private Set<Author> authors = new HashSet<>();
+
+    @ManyToOne
+    @JoinColumn(name = "publisher_id", nullable = false)
+    private Publisher publisher;
+
+    @ManyToMany
+    @JoinTable(
+            name = "book_category",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
+}
