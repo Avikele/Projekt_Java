4d102d169111603864ef86876e7931366f1d2df6
Avikele <konrad.malarczyk1@gmail.com>
2024-11-06 12:43:23 +0100

    /feat add author entity

---
+++ ems-backend/src/main/java/com/projekt/ems/entity/Author.java
@@ -0,0 +1,27 @@
+package com.projekt.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "authors")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+}
