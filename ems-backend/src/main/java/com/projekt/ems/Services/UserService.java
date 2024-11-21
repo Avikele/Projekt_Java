52a56439de4da4f84c38c0e231af0b0f5bcdf19f
Konrad Malarczyk <konrad.malarczyk1@gmail.com>
2024-11-21 17:05:27 +0100

    Add user overall statistics

--- ems-backend/src/main/java/com/projekt/ems/Services/UserService.java
+++ ems-backend/src/main/java/com/projekt/ems/Services/UserService.java
@@ -1,7 +1,9 @@
 package com.projekt.ems.Services;
 
 import com.projekt.ems.Dto.UserDto;
+import com.projekt.ems.Models.User;
 
+import java.time.LocalTime;
 import java.util.List;
 
 public interface UserService {
@@ -10,4 +12,10 @@ public interface UserService {
     UserDto saveUser(UserDto user);
     UserDto updateUser(Long id, UserDto user);
     void deleteUser(Long id);
+    void addTime(LocalTime time, User user);
+    void removeTime(LocalTime time, User user);
+    void addReadBook(User user);
+    void removeReadBook(User user);
+    void addPagesRead(Integer pages, User user);
+    void removePagesRead(Integer pages, User user);
 }
